003e1551612a1f2cc59ae17640d04b83fc0e56f4
PSP0929 <102664805+PSP0929@users.noreply.github.com>
2025-01-07 12:16:21 +0900

    Update Player.java

--- game/Player.java
+++ game/Player.java
@@ -5,7 +5,6 @@ import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import java.io.FileInputStream;
 
 public class Player {
     private int x;
